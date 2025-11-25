7b9513243d0523331ffe0417965468ee0401b267
The3hats <61612035+The3hats@users.noreply.github.com>
2025-11-25 10:46:33 +0530

    Create newntlm.bat

---
+++ newntlm.bat
@@ -0,0 +1,5 @@
+Started interactive SMB client shell via TCP
+Service Installed.. CONNECT!
+Done dumping SAM hashes for host:
+DA already added. Refusing to add another
+Domain info dumped into lootdir!
